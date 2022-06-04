40459111c45c109bb29b609cdfcea5188c51a2aa
Gusta <gustavowustemberg14@gmail.com>
2022-06-04 20:53:46 -0300

    Create conversor-native

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert} from 'react-native';
 
 export default function App() {
+  function teste(){
+    Alert.alert('testado');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>🥶 Aplicativo Temperatura 🥵</Text>
+      <TextInput style={styles.inputTemperature} 
+      placeholder='Informe a temperatura em celsius'
+      keyboardType='numeric'/>
+
+      <TouchableOpacity onPress={teste}>
+      <Text style={styles.buttonCalculed}>Calcular</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -13,8 +23,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#87CEFA',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    textAlign: 'center',
+    marginTop: 40,
+    marginBottom: 40,
+    fontSize: 22,
+    color: '#FFF',
+  },
+  buttonCalculed: {
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputTemperature: {
+    backgroundColor: '#fff',
+    color: '#000',
+    margin: 15,
+    padding: 10,
+    borderColor: '#000',
+    borderRadius: 10,
+    borderWidth: 1,
+  }
 });
